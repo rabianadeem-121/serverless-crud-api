aa7e13d83ba8aa08d81c702c8408e4b142d44c1b
rabianadeem-121 <rn.aibar.rn@gmail.com>
2025-12-11 19:22:21 +0500

    Use Docker for tests with SSH tunnel to private RDS

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,7 @@
 variable "aws_region" { default = "ap-south-1" }
 variable "vpc_cidr" { default = "10.0.0.0/16" }
 
-variable "db_username" { default = "admin" }
+variable "db_username" { default = "appuser" }
 variable "db_password" { default = "ChangeMe123!" }
 variable "instance_type" { default = "t3.micro" }
 variable "public_key" {
