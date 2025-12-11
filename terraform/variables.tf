2368a52806b0d1aa7158f21d484183c50f26360c
rabianadeem-121 <rn.aibar.rn@gmail.com>
2025-12-11 19:15:09 +0500

    Use Docker for tests with SSH tunnel to private RDS

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,18 +1,13 @@
 variable "aws_region" { default = "ap-south-1" }
 variable "vpc_cidr" { default = "10.0.0.0/16" }
-variable "public_subnet_cidr" { default = "10.0.1.0/24" }
-variable "private_subnet_cidr" { default = "10.0.2.0/24" }
+
 variable "db_username" { default = "admin" }
 variable "db_password" { default = "ChangeMe123!" }
 variable "instance_type" { default = "t3.micro" }
 variable "public_key" {
   type = string
 }
-variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
-  type        = string
-  default     = "10.0.0.0/16"
-}
+
 
 variable "public_subnet_cidr_a" {
   description = "CIDR block for public subnet in AZ a"
@@ -38,14 +33,3 @@ variable "private_subnet_cidr_b" {
   default     = "10.0.11.0/24"
 }
 
-variable "db_username" {
-  description = "Master username for RDS database"
-  type        = string
-  sensitive   = true
-}
-
-variable "db_password" {
-  description = "Master password for RDS database"
-  type        = string
-  sensitive   = true
-}
