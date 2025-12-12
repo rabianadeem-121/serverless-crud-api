a01c8c147614b925564bff21e7abf6f3084fcd3b
rabianadeem-121 <rn.aibar.rn@gmail.com>
2025-12-12 18:58:03 +0500

    Reapply CI/CD workflow changes

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,31 +1,65 @@
 variable "aws_region" {
-  description = "AWS Region"
+  description = "AWS region"
   default     = "ap-south-1"
 }
 
-variable "db_username" {
-  description = "RDS username"
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr_a" {
+  description = "CIDR block for public subnet in AZ a"
+  default     = "10.0.1.0/24"
+}
+
+variable "public_subnet_cidr_b" {
+  description = "CIDR block for public subnet in AZ b"
+  default     = "10.0.2.0/24"
+}
+
+variable "private_subnet_cidr_a" {
+  description = "CIDR block for private subnet in AZ a"
+  default     = "10.0.10.0/24"
+}
+
+variable "private_subnet_cidr_b" {
+  description = "CIDR block for private subnet in AZ b"
+  default     = "10.0.11.0/24"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  default     = "t3.micro"
+}
+
+variable "public_key" {
+  description = "EC2 public key for SSH"
   type        = string
+}
+
+variable "db_username" {
+  description = "RDS database username"
+  type        = string
+  default = "appuser"
 }
 
 variable "db_password" {
-  description = "RDS password"
+  description = "RDS database password"
   type        = string
   sensitive   = true
+  default = "ChangeMe123!"
 }
 
 variable "db_name" {
-  description = "Database name"
+  description = "RDS database name"
+  type        = string
+  default = "cruddb" 
+}
+
+variable "lambda_image_uri" {
+  description = "Docker image URI for Lambda"
   type        = string
 }
 
-variable "lambda_function_name" {
-  description = "Lambda function name"
-  type        = string
-}
-
-variable "docker_image_tag" {
-  description = "Docker image tag"
-  type        = string
-  default     = "latest"
-}
+variable "lambda_function_name" { default = "crud-lambda" }
