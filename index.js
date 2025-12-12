bfeb201b79c7750a0386d89e5d046d33fccb9b98
rabianadeem-121 <rn.aibar.rn@gmail.com>
2025-12-12 15:27:35 +0500

    edited yml

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const { Client } = require('pg');
+const { handler } = require('../index');
+
 
 exports.handler = async (event) => {
     // Force IPv4 for SSH tunnel
     const client = new Client({
-        host: process.env.DB_HOST || '127.0.0.1', // default to 127.0.0.1
+        host: process.env.DB_HOST,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
